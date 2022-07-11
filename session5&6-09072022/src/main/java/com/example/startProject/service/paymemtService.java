a7e69556a75a396f4433ba86ec2c2ae4026e68b3
fenildev <fenil.shah@pinelabs.com>
2022-07-11 10:36:35 +0530

    session6 classwork

---
+++ session5&6-09072022/src/main/java/com/example/startProject/service/paymemtService.java
@@ -0,0 +1,5 @@
+package com.example.startProject.service;
+
+public interface paymemtService {
+
+}
---
+++ session5&6-09072022/src/main/java/com/example/startProject/serviceImpl/PaymentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.startProject.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.startProject.service.paymemtService;
+
+@Service
+public class PaymentServiceImpl implements paymemtService{
+
+}
